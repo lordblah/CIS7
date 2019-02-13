d2ca504f791deea4790a77ac1600b6a6731c4ca4
Jonathan Hernandez <helharatheband@gmail.com>
2019-02-13 13:41:03 -0800

    Add files via upload
    Example of STD

---
+++ Chapter 1/hexadec/main.cpp
@@ -0,0 +1,15 @@
+//Programe to illustrate the use of the std of oct hex and dec
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //the number that is going to be translated
+    int num = 128;
+
+    cout << oct << "oct:" << num << endl;
+    cout << dec << "dec:" << num << endl;
+    cout << hex << "hex:" << num << endl;
+    return 0;
+}
